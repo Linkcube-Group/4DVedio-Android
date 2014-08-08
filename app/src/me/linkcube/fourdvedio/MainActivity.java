29b26de13a665508486195d1009849b183a5f0bb
Ervin Wang <orange.work@qq.com>
2014-08-08 16:57:17 +0800

    Update package name

--- app/src/me/linkcube/fourdvedio/MainActivity.java
+++ app/src/me/linkcube/fourdvedio/MainActivity.java
@@ -1,9 +1,8 @@
-package me.linkcube.fourdvedio;
+package me.linkcube.FourDVedio;
 
+import me.linkcube.FourDVedio.R;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
